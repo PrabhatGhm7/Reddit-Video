2751199686a8230f30673d2d70f80413308524dc
PrabhatGhm7 <103963065+PrabhatGhm7@users.noreply.github.com>
2024-04-28 20:35:29 +0545

    Update credentails.py

--- credentails.py
+++ credentails.py
@@ -1,7 +1,7 @@
 import praw
 reddit = praw.Reddit(
-        client_id = "EEY8xwLR1ANQ21kM94TkWQ",
-        client_secret = "xIYIWpAEem4m47_PC7GKKxhY7k3L5Q",
-        username = "Tiny-Zookeepergame62",
-        user_agent = "Prabhat Ghimire u/m runthat"
+        client_id = "",
+        client_secret = "",
+        username = "",
+        user_agent = ""
 )
